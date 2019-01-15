c9c53184eccca4a04997fad60a862ed6dbe1a43e
John Azzaro <jazzaro@temple.edu>
2019-01-15 13:06:31 -0800

    additions to models and server.js

--- models.js
+++ models.js
@@ -3,7 +3,29 @@ const mongoose = require('mongoose');
 const blogPostSchema = mongoose.Schema({
     title: {type: String, required: true},
     author: { 
-        firstName: String, lastName: String,
+        firstName: String, 
+        lastName: String,
     },
     content: String, 
+});
+
+blogPostSchema.virtual("authorName").get(function() {
+    return `${this.author.firstName} ${this.author.lastName}`.trim();
 })
+
+
+// below we take blogpostschema, adding a serialize to its methods
+blogPostSchema.methods.serialize = function() {
+    return {
+            title: this.title, 
+            content: this.content,
+            author: this.authorName,
+            created: this._id.getTimestamp(),
+        
+    }
+}
+
+// always make the collection name the same as the model name.
+const BlogPost = mongoose.model("BlogPost", blogPostSchema);
+
+module.exports = {BlogPost}
--- server.js
+++ server.js
@@ -1,1 +1,5 @@
 const express = require("express");   // the reason why we can just put express is becasue its a node module.
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+const {BlogPost} = require("./models");
