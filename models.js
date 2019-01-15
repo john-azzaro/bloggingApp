79b56e183d123e0b67f2f540dbdf16e16f5067d1
John Azzaro <jazzaro@temple.edu>
2019-01-15 12:41:05 -0800

    created blogPostSchema

--- models.js
+++ models.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+
+const blogPostSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    author: { 
+        firstName: String, lastName: String,
+    },
+    content: String, 
+})
