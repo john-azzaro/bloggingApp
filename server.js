6b4dc4fa9d3e6023953736077897a856497c42b1
John Azzaro <jazzaro@temple.edu>
2019-01-18 10:44:30 -0800

    server.js psudocode

--- server.js
+++ server.js
@@ -8,3 +8,23 @@ const app = express();
 
 mongoose.Promise = global.Promise;
 const {BlogPost} = require("./models");
+
+
+
+// sends back all posts to database.
+app.get('/posts', (req, res) => {});
+
+// sends back a single blog post with :id if it exists, using schema below.
+app.get('/posts/:id', (req, res) => {});
+
+// blog post schema
+BlogPost.create({});
+
+// 
+app.post('/posts', (req, res) => {});
+
+app.put('/posts/:id', (req, res) => {});
+
+app.delete('/posts/:id', (req, res) => {});
+
+app.delete('/:id', (req, res) => {});
